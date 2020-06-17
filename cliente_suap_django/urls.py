8f2bad8f17285aa3cc92db5ff8ea3620d79f82f3
Jurandy Soares <jurandy.soares@ifrn.edu.br>
2020-06-17 20:17:49 -0300

    App renomeada para Mel

--- cliente_suap_django/urls.py
+++ cliente_suap_django/urls.py
@@ -1,4 +1,4 @@
-"""cliente_suap_django URL Configuration
+"""mel URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
